79cbdc513b5b5b435f44691e57587f53bc2fe925
RaptorialThing <ruzal.gabdrahmanoff@yandex.ru>
2021-05-22 03:46:50 +0300

    test create and load worker

--- Commands/Logistics/RegisterWorkerCommand.php
+++ Commands/Logistics/RegisterWorkerCommand.php
@@ -157,6 +157,8 @@ class RegisterWorkerCommand extends UserCommand
                     $text = 'error fetching from database';
                 } 
 
+                $result = $text;
+
                 $this->conversation->update();
                 unset($notes['state']);
                 $this->conversation->stop();
