faae5bcc15374961d542463ef6f3312ab6d175a5
RaptorialThing <ruzal.gabdrahmanoff@yandex.ru>
2021-05-22 03:41:15 +0300

    test create and load worker

--- Commands/Logistics/RegisterWorkerCommand.php
+++ Commands/Logistics/RegisterWorkerCommand.php
@@ -174,7 +174,7 @@ class RegisterWorkerCommand extends UserCommand
         } 
 
         return $this->replyToChat($text);*/
-        return $this->replyToChat($text);
+        //return $this->replyToChat($text);
     }
 
 
